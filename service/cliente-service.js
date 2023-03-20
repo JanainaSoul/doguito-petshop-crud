7b2854b377532874f4b9d002147819a8336f0a4f
Janaina <janainasoul@icloud.com>
2023-03-20 17:17:00 -0300

    aprendendo a fazer promise no js

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -19,19 +19,54 @@ const criaNovaLinha = (nome, email) => {
 //pegando a tabela
 const tabela = document.querySelector('[data-tabela]')
 
-const http = new XMLHttpRequest()
 
-http.open('GET', 'http://localhost:3000/profile'); //GET p/ pegar os dados do servidor - 2 argumentos 1° o q vc quer pedir pro servidor e o 2° é p onde vou enviar a requisiçao
 
-http.send()
-
-//metodo onload
-http.onload = () => {
-    const data = JSON.parse(http.response)
-    console.log(data) 
-    //data.forEach(elemento => {
-        //tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))// para cada cliente
-    //})
+const listaClientes = () =>{
+    const promise = new Promise((resolve, reject)=>{
+        const http = new XMLHttpRequest()
+        http.open('GET', 'http://localhost:3000/profile'); //GET p/ pegar os dados do servidor - 2 argumentos 1° o q vc quer pedir pro servidor e o 2° é p onde vou enviar a requisiçao
+    
+    
+    http.onload = () => {
+        if(http.status >= 400){
+            reject(JSON.parse(http.response))
+        }else{
+            resolve(JSON.parse(http.response))
+            }
+        }
+        http.send()
+    })
+    //console.log(promise)
+    return promise
 }
 
+listaClientes() //fazendo uma promessa - qdo for compeltada vai retornar os dados
+.then(data =>{
+    //const data = (http.response)
+    //console.log(data) 
+    data.forEach(elemento => {
+        tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))// para cada cliente
+    })
+})
 
+
+
+
+
+
+//     const http2 = new XMLHttpRequest()
+//     http2.open('GET', 'http://localhost:3000/profile/semanaPassada');
+
+//     http2.onload = () =>{
+//         ..
+
+//         const http3 = new XMLHttpRequest()
+//             http3.open('GET', 'http://localhost:3000/profile/semanaRetrasada')
+        
+//             http3.onload = () =>{ - função auxiliar qdo uma função assincrona é chamada - call back
+
+//             }
+//             http3.send() 
+//     }
+//     http2.send()
+// } -> dificuldade de entender , dificil manuntenção
