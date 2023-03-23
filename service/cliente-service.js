00e333d3e2bd94ad2459a2e6f2730165a17ccaab
Janaina <janainasoul@icloud.com>
2023-03-23 17:22:11 -0300

    criando função para incluir Novo Cliente

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -7,8 +7,27 @@ const listaClientes = () =>{
     }) // metodo global da API ela ja faz um get e a promise
 }
 
+const criaClente = (nome,email) =>{
+    return fetch('http://localhost:3000/profile', {
+        method:'POST',
+        headers: {
+            'contet-type': 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome, 
+            email: email
+        })
+                
+    })
+    .then(resposta =>{
+        return resposta.body
+    })
+}   
+
+
 export const clienteService ={
-    listaClientes
+    listaClientes,
+    criaCliente
 }
 
 
